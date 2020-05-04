67016cdfebb913bf1b4c4f58befb3df20e47b6da
Shreyans Sonthalia <ssshreyans26@gmail.com>
2020-05-04 12:44:49 +0530

    some correctionon vendoe

--- routes/routes.js
+++ routes/routes.js
@@ -65,9 +65,10 @@ router.post('/login', async (req, res, next) => {
   });
 
   router.get('/vendor_products',async(req,res,next)=>{
-    var brand = BrandModel.find()
+    // var brand = BrandModel.find()
     ProductModel.find().populate('subcategory').exec((err,product)=>{
-      res.render('vendor_product',{Category:product,brand:brand});
+      
+      res.render('vendor_product',{Category:product});
     })
     
   })
