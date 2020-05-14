f7e053c0221fccc4ca31509d3a1d6c5f60852413
Shreyans Sonthalia <ssshreyans26@gmail.com>
2020-05-14 23:48:12 +0530

    added products to customer page

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+const ProductModel = require('../model/product');
+const SubCategoryModel = require("../model/subcategory");
+const BrandModel = require("../model/brand.js");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('home');
 });
 
 router.get('/customer',function(req,res,next){
-  res.render('customer')
+  ProductModel.find().populate('subcategory').exec((err,product)=>{
+    BrandModel.find((err,brand)=>{
+      res.render('customer',{brand:brand,product:product})
+      // res.send({product})
+    })
+  })
+  
 })
 
 
